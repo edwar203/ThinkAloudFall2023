2fdfa97435c64abea6265d2c39f6274a5c9afb75
edwar203 <127564906+edwar203@users.noreply.github.com>
2023-09-15 05:15:06 +0000

    TODO

---
+++ interactive_story.py
@@ -0,0 +1,12 @@
+"""
+    Name: interactive_story.py
+    Author: Bill Edwards
+    Created: 09/14/2023
+    Purpose: To use Python to create a simple interactive story like a goosebumps book
+"""
+
+#TODO Title
+#Introduction with first decision
+#Create multiple def with different story paths
+#Create meat of story with multiple decisions leading to multiple outcomes some shorter then others
+#Write multiple story closings
